c959cf463a794876469d3a354f490bb83cb1a9d6
Md.Al-Amin <amin.se.aiub@gmail.com>
2025-05-25 12:03:47 +0600

    Add files via upload

---
+++ ajax/abc.php
@@ -0,0 +1,7 @@
+<?php
+    //sleep(3);
+    //$json = $_POST['json'];
+    //$user = json_decode($json);
+    $user = ['username'=>'alamin', 'email'=>'alamin@aiub.edu', 'password'=>'123'];
+    echo json_encode($user);
+?>
